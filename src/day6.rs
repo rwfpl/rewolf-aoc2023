80b171c00d8cda06a2b77a0a251aaa01b73ab2c1
ReWolf <rwfpl@users.noreply.github.com>
2023-12-06 21:39:23 +0100

    day6

---
+++ src/day6.rs
@@ -0,0 +1,61 @@
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+
+struct Race {
+    time: u64,
+    distance: u64,
+}
+
+impl Race {
+    fn from(time: u64, distance: u64) -> Self {
+        Self { time, distance }
+    }
+
+    fn hold(&self, seconds: u64) -> u64 {
+        (self.time - seconds) * seconds
+    }
+
+    fn get_win_number(&self) -> usize {
+        (1..self.time)
+            .filter(|t| self.hold(*t) > self.distance)
+            .count()
+    }
+}
+
+fn solution(input: &[Race]) -> usize {
+    input
+        .par_iter()
+        .map(|race| race.get_win_number())
+        .product::<usize>()
+}
+
+#[test]
+fn test_run() {
+    assert_eq!(
+        solution(&[Race::from(7, 9), Race::from(15, 40), Race::from(30, 200)]),
+        288
+    );
+    assert_eq!(solution(&[Race::from(71530, 940200)]), 71503);
+    assert_eq!(
+        solution(&[
+            Race::from(48, 296),
+            Race::from(93, 1928),
+            Race::from(85, 1236),
+            Race::from(95, 1391),
+        ]),
+        2756160
+    );
+    assert_eq!(solution(&[Race::from(48938595, 296192812361391)]), 34788142);
+}
+
+pub fn run() -> (String, String) {
+    (
+        solution(&[
+            Race::from(48, 296),
+            Race::from(93, 1928),
+            Race::from(85, 1236),
+            Race::from(95, 1391),
+        ])
+        .to_string(),
+        solution(&[Race::from(48938595, 296192812361391)]).to_string(),
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
     let days = [
@@ -20,6 +21,7 @@ fn main() {
         (3, day3::run),
         (4, day4::run),
         (5, day5::run),
+        (6, day6::run),
     ];
     let now = Instant::now();
     let day = env::args()
